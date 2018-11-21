6c9962fc885b38356c42175d07543e660a2cee1b
Amy Gillies <2254790g@student.gla.ac.uk>
2018-11-21 13:25:12 +0000

    fixing the settings and views to run the templates on the server

--- runmate/views.py
+++ runmate/views.py
@@ -2,13 +2,15 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+
 
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Start page for RunMate")
+    return render(request, 'runmate/index.html', {})
 
 def dataView(request):
-    return HttpResponse("Loaded info on page for RunMate")
+    return render(request, 'runmate/loadedData.html', {})
--- runmate_project/settings.py
+++ runmate_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'runmate_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
